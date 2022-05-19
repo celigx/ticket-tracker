a6d0344929ee12ffc9b4a7bc4d3747ec568b7865
Celig <github@cela.anonaddy.com>
2022-05-19 18:02:31 +0200

    Add total balance component

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import TotalBalance from './components/TotalBalance';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TotalBalance />
       <StatusBar style="auto" />
     </View>
   );
@@ -12,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EBEBEB',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/TotalBalance.js
@@ -0,0 +1,34 @@
+import { StyleSheet, StatusBar, View, Text } from 'react-native';
+
+const TotalBalance = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ukupno raspoloživo</Text>
+      <Text style={styles.balance}>200,00 kn</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#181818',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    borderRadius: 6,
+    padding: 10,
+    marginTop: StatusBar.currentHeight
+  },
+  title: {
+    color: '#DADADA',
+    fontSize: 24
+  },
+  balance: {
+    color: '#FFFFFF',
+    fontSize: 36,
+    fontWeight: 'bold',
+  }
+});
+
+export default TotalBalance;
